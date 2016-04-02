4179df23ac1832cb9252280ebf8b18f93308e05d
jumptigerJ <zhangyanj_light@163.com>
2016-04-02 13:58:42 +0800

    MVN_test

---
+++ test/src/main/java/myTest/Atest.java
@@ -0,0 +1,12 @@
+package myTest;
+
+public class Atest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.print("I="+i+" ");
+		}
+	}
+
+}
